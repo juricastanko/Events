17883c2249f38e5fc255e1f4daf094b34e1ada38
Jurica Stanko <stanko64@icloud.com>
2017-03-23 07:18:31 -0100

    Add files via upload

---
+++ niz.js
@@ -0,0 +1,28 @@
+var niz=["jabuka","kruska","banana","sljiva","orah","ljesnjak","krumpir"];
+for(var i=0; i<7; i++){
+    var tag=" ";
+    tag+=tag+niz[i];
+
+}
+if(niz[i]=="banana"){
+    document.getElementById("ptag").innerHTML+=niz.lenght;
+
+
+}
+document.getElementById("ptag").innerHTML=tag;
+console.log(tag);
+
+function fankcija(){
+    unos=document.getElementById("unos").value;
+
+}
+
+var bob={
+    name:"bobe",age:30
+};
+var susan={
+    name:"susane",age:0
+};
+bob.do
+console.log(susan.age,bob.name);
+
